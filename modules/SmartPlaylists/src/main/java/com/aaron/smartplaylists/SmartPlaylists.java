effd21fd6c943bf69a8adfcc3bba5b02393876a1
Aaron Rodriguez <adashrod@gmail.com>
2014-09-05 18:03:42 -0700

    renaming EntryPoint to SmartPlaylists

--- modules/SmartPlaylists/src/main/java/com/aaron/smartplaylists/SmartPlaylists.java
+++ modules/SmartPlaylists/src/main/java/com/aaron/smartplaylists/SmartPlaylists.java
@@ -4,7 +4,7 @@ package com.aaron.smartplaylists;
  * This is the entry point of the program. It decides whether to pass command line arguments to the command line program
  * or to start the GUI
  */
-public class EntryPoint {
+public class SmartPlaylists {
     public static void main(final String[] arguments) {
         if (arguments.length > 0) {
             CommandLineConverter.run(arguments);
